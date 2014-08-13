5219ce4387437ff01235b1658fb44127b1337ca0
Danny Fallon <danny@intercom.io>
2014-08-13 18:28:37 +0100

    Make metrics dependent on services

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,54 +1,51 @@
 class MetricsController < ApplicationController
   before_action :set_metric, only: [:show, :edit, :update, :destroy]
+  before_action :set_service
 
-  # GET /metrics
   def index
-    @metrics = Metric.all
+    @metrics = @service.metrics.all
   end
 
-  # GET /metrics/1
   def show
   end
 
-  # GET /metrics/new
   def new
-    @metric = Metric.new
+    @metric = @service.metrics.new
   end
 
-  # GET /metrics/1/edit
   def edit
   end
 
-  # POST /metrics
   def create
-    @metric = Metric.new(metric_params)
+    @metric = @service.metrics.new(metric_params)
 
     if @metric.save
-      redirect_to @metric, notice: 'Metric was successfully created.'
+      redirect_to [@service, @metric], notice: 'Metric was successfully created.'
     else
       render :new
     end
   end
 
-  # PATCH/PUT /metrics/1
   def update
     if @metric.update(metric_params)
-      redirect_to @metric, notice: 'Metric was successfully updated.'
+      redirect_to [@service, @metric], notice: 'Metric was successfully updated.'
     else
       render :edit
     end
   end
 
-  # DELETE /metrics/1
   def destroy
     @metric.destroy
-    redirect_to metrics_url, notice: 'Metric was successfully destroyed.'
+    redirect_to service_metrics_url, notice: 'Metric was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_metric
       @metric = Metric.find(params[:id])
+    end
+
+    def set_service
+      @service = Service.find(params[:service_id])
     end
 
     # Only allow a trusted parameter "white list" through.
--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Metric, :type => :model do
   it { should validate_presence_of(:title) }
   it { should ensure_inclusion_of(:datapoint_source).in_array(Metric::DATAPOINT_SOURCE_VALUES) }
   it { should validate_presence_of(:datapoint_name) }
+  it { should belong_to(:service) }
 end
