af7785b89b3836c4aa17800a707dcd43818c67c8
Danny Fallon <danny@intercom.io>
2014-08-13 15:42:41 +0100

    Nest metric routes in a service

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   github_authenticate(org: ENV['GITHUB_ORG']) do
     root to: redirect(path: '/services', status: 302)
 
-    resources :services
-    resources :metrics
+    resources :services do
+      resources :metrics
+    end
   end
 end
