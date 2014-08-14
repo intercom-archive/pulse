416eb5941b6bad3a8edc4320be2308f808238d98
Danny Fallon <danny@intercom.io>
2014-08-14 16:45:11 +0100

    Add the lib directory to Rails autoload path

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Pulse
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add the lib dir to the autoload path
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
