578a6999f7a1b5b6119e8cffdc75af9196f10372
Danny Fallon <danny@intercom.io>
2014-08-15 11:55:08 +0100

    Add validation rules to the Metric model for cloudwatch metrics

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,13 +1,21 @@
 class Metric < ActiveRecord::Base
-  DATAPOINT_SOURCE_VALUES = %w(graphite)
+  DATAPOINT_SOURCE_VALUES = %w(graphite cloudwatch).freeze
+  CLOUDWATCH_NAMESPACES = %w(AWS/ELB AWS/RDS AWS/SNS AWS/SQS).freeze
 
   validates :title, presence: true
   validates :datapoint_source, inclusion: { in: DATAPOINT_SOURCE_VALUES }
   validates :datapoint_name, presence: true
 
-  belongs_to :service
+  validates :cloudwatch_namespace, inclusion: { in: CLOUDWATCH_NAMESPACES }, if: :cloudwatch_metric?
+  validates :cloudwatch_identifier, presence: true, if: :cloudwatch_metric?
+
+  def cloudwatch_metric?
+    datapoint_source == 'cloudwatch'
+  end
 
   def graphite_data
     @graphite_data ||= GraphiteMetric.new(title, datapoint_name).datapoints
   end
+
+  belongs_to :service
 end
